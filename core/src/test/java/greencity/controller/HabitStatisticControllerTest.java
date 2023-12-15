13decf58d3dba2a81e45d319d085ef76eea455b4
IvanovNikita <nikita.ivanov@bearg.net>
2023-12-15 18:27:21 +0200

    Refactor HabitStatisticControllerTest

--- core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
+++ core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
@@ -16,7 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
