1caea74813c67aaa3b4ee64b4650242e43e0d27f
Vlada <proskurina.vlada@gmail.com>
2023-12-18 11:24:16 +0200

    Fixes HabitStatisticControllerTest 251

--- core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
+++ core/src/test/java/greencity/controller/HabitStatisticControllerTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,10 +33,9 @@ class HabitStatisticControllerTest {
     HabitStatisticService habitStatisticService;
     @InjectMocks
     HabitStatisticController habitStatisticController;
-
     ObjectMapper objectMapper = new ObjectMapper();
-
-    long existId = 1;
+    private static final long existId = 1;
+    private static final String habitStatisticResource = "/habit/statistic";
 
     @BeforeEach
     void setUp() {
@@ -50,12 +48,11 @@ class HabitStatisticControllerTest {
         GetHabitStatisticDto habitStatisticDto = new GetHabitStatisticDto();
         habitStatisticDto.setHabitStatisticDtoList(Collections.singletonList(new HabitStatisticDto()));
 
-        when(habitStatisticService.findAllStatsByHabitId(1L)).thenReturn(habitStatisticDto);
-        mockMvc.perform(get("/habit/statistic/{habitId}", existId));
+        when(habitStatisticService.findAllStatsByHabitId(existId)).thenReturn(habitStatisticDto);
+        mockMvc.perform(get(habitStatisticResource + "/{habitId}", existId))
+                .andExpect(status().isOk());
 
         ResponseEntity<GetHabitStatisticDto> response = habitStatisticController.findAllByHabitId(existId);
-
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertEquals(habitStatisticDto, response.getBody());
     }
 
@@ -64,11 +61,11 @@ class HabitStatisticControllerTest {
         List<HabitStatisticDto> habitStatisticDtoList = Collections.singletonList(new HabitStatisticDto());
 
         when(habitStatisticService.findAllStatsByHabitAssignId(existId)).thenReturn(habitStatisticDtoList);
-        mockMvc.perform(get("/assign/{habitAssignId}", existId));
+        mockMvc.perform(get(habitStatisticResource + "/assign/{habitAssignId}", existId))
+                .andExpect(status().isOk());
 
-        ResponseEntity<List<HabitStatisticDto>> response = habitStatisticController.findAllStatsByHabitAssignId(existId);
-
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        ResponseEntity<List<HabitStatisticDto>> response =
+                habitStatisticController.findAllStatsByHabitAssignId(existId);
         Assertions.assertEquals(habitStatisticDtoList, response.getBody());
     }
 
@@ -85,15 +82,14 @@ class HabitStatisticControllerTest {
         lenient().when(habitStatisticService.saveByHabitIdAndUserId(eq(existId), anyLong(), eq(addHabitStatisticDto)))
                 .thenReturn(habitStatisticDto);
 
-
         objectMapper.registerModule(new JavaTimeModule());
         String content = objectMapper.writeValueAsString(addHabitStatisticDto);
 
-        mockMvc.perform(post("/habit/statistic/{habitId}", existId)
+        mockMvc.perform(post(habitStatisticResource +"/{habitId}", existId)
+                        .principal(ModelUtils.getPrincipal())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content))
                 .andExpect(status().is(201));
-
         addHabitStatisticDto =
                 objectMapper.readValue(content, AddHabitStatisticDto.class);
         verify(habitStatisticService).saveByHabitIdAndUserId(eq(existId), nullable(Long.class), eq(addHabitStatisticDto));
@@ -101,14 +97,16 @@ class HabitStatisticControllerTest {
 
     @Test
     void updateStatistic() throws Exception {
-        UpdateHabitStatisticDto updateHabitStatisticDto = new UpdateHabitStatisticDto(ModelUtils.addHabitStatisticDto().getAmountOfItems(), ModelUtils.addHabitStatisticDto().getHabitRate());
+        UpdateHabitStatisticDto updateHabitStatisticDto = new UpdateHabitStatisticDto(
+                ModelUtils.addHabitStatisticDto().getAmountOfItems(),
+                ModelUtils.addHabitStatisticDto().getHabitRate());
 
         lenient().when(habitStatisticService.update(eq(existId), anyLong(), eq(updateHabitStatisticDto)))
                 .thenReturn(updateHabitStatisticDto);
 
         String content = objectMapper.writeValueAsString(updateHabitStatisticDto);
 
-        mockMvc.perform(put("/habit/statistic/{habitId}", existId)
+        mockMvc.perform(put(habitStatisticResource + "/{habitId}", existId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content))
                 .andExpect(status().is(200));
@@ -123,12 +121,12 @@ class HabitStatisticControllerTest {
 
         when(habitStatisticService.getTodayStatisticsForAllHabitItems(locale.getLanguage()))
         .thenReturn(listHabitItemsAmountStatisticDto);
-        mockMvc.perform(get("/todayStatisticsForAllHabitItems"));
+        mockMvc.perform(get(habitStatisticResource + "/todayStatisticsForAllHabitItems"))
+                .andExpect(status().isOk());
 
         ResponseEntity<List<HabitItemsAmountStatisticDto>> response =
                 habitStatisticController.getTodayStatisticsForAllHabitItems(locale);
 
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertEquals(listHabitItemsAmountStatisticDto, response.getBody());
     }
 
@@ -139,12 +137,12 @@ class HabitStatisticControllerTest {
 
         when(habitStatisticService.getAmountOfAcquiredHabitsByUserId(userId))
                 .thenReturn(amountOfAcquiredHabits);
-        mockMvc.perform(get("/acquired/count"));
+        mockMvc.perform(get(habitStatisticResource + "/acquired/count?userId="+ userId))
+                .andExpect(status().isOk());
 
         ResponseEntity<Long> response =
                 habitStatisticController.findAmountOfAcquiredHabits(userId);
 
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertEquals(amountOfAcquiredHabits, response.getBody());
     }
 
@@ -155,12 +153,12 @@ class HabitStatisticControllerTest {
 
         when(habitStatisticService.getAmountOfHabitsInProgressByUserId(userId))
                 .thenReturn(amountOfHabitsInProgress);
-        mockMvc.perform(get("/acquired/count"));
+        mockMvc.perform(get(habitStatisticResource + "/acquired/count?userId="+ userId))
+                .andExpect(status().isOk());
 
         ResponseEntity<Long> response =
                 habitStatisticController.findAmountOfHabitsInProgress(userId);
 
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
         Assertions.assertEquals(amountOfHabitsInProgress, response.getBody());
     }
 }
