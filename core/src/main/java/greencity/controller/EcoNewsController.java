dd39115836e822efdd7a23231e974d46d7297698
oleksandr.kobets.dev@gmail.com <oleksandr.kobets.dev@gmail.com>
2023-12-18 23:38:39 +0200

    342 - EcoNewsController.java - uploadImages() - 401 status added

--- core/src/main/java/greencity/controller/EcoNewsController.java
+++ core/src/main/java/greencity/controller/EcoNewsController.java
@@ -113,6 +113,7 @@ public class EcoNewsController {
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = HttpStatuses.CREATED,
                     response = String.class),
+            @ApiResponse(code = 401, message = HttpStatuses.UNAUTHORIZED),
             @ApiResponse(code = 403, message = HttpStatuses.FORBIDDEN),
             @ApiResponse(code = 415, message = HttpStatuses.UNSUPPORTED_MEDIA_TYPE)
     })
