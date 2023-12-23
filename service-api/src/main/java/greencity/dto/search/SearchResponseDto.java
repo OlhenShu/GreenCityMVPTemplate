a54a77647a6451209461af74376fd13fd50ba1fd
staaankey <dimaclopov@gmail.com>
2023-12-23 23:33:01 +0200

    deleted extra reports files

--- service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
+++ service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
@@ -1,20 +1,5 @@
 package greencity.dto.search;
 
-//import lombok.Builder;
-//import lombok.EqualsAndHashCode;
-//import lombok.Getter;
-//
-//import java.util.List;
-//
-//@Getter
-//@Builder
-//@EqualsAndHashCode
-//public class SearchResponseDto {
-//    List<SearchNewsDto> ecoNews;
-//
-//    Long countOfResults;
-//}
-
 import lombok.Builder;
         import lombok.EqualsAndHashCode;
         import lombok.Getter;
