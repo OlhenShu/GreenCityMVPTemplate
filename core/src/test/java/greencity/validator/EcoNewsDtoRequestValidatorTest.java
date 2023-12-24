f00566621859b35578562567c7f8385700f7244b
Bogdan <veremienko.bogdan@gmail.com>
2023-12-24 15:05:21 +0200

    unit test to coewstoequestalidator

--- core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -15,7 +15,6 @@ import java.util.Collections;
 import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 @ExtendWith(MockitoExtension.class)
 class EcoNewsDtoRequestValidatorTest {
     @InjectMocks
