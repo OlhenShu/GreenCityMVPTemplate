834d8f490d3f62f14b84edc0b5e36651693c5973
Bogdan <veremienko.bogdan@gmail.com>
2023-12-24 15:01:32 +0200

    unit test to coewstoequestalidator

--- core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
+++ core/src/test/java/greencity/validator/EcoNewsDtoRequestValidatorTest.java
@@ -1,18 +1,28 @@
 package greencity.validator;
 
+import greencity.constant.ValidationConstants;
 import greencity.dto.econews.AddEcoNewsDtoRequest;
+import greencity.exception.exceptions.WrongCountOfTagsException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static greencity.ModelUtils.getAddEcoNewsDtoRequest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+
+@ExtendWith(MockitoExtension.class)
 class EcoNewsDtoRequestValidatorTest {
     @InjectMocks
     private EcoNewsDtoRequestValidator validator;
+
+    @Mock
+    private UrlValidator urlValidator;
 
     @Test
     void isValidTrueTest() {
@@ -20,4 +30,57 @@ class EcoNewsDtoRequestValidatorTest {
         request.setSource("https://eco-lavca.ua/");
         assertTrue(validator.isValid(request, null));
     }
+
+    @Test
+    void isValidEmptySourceTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource("");
+
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidNullSourceTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setSource(null);
+
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidWrongCountOfTagsExceptionTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setTags(Collections.nCopies(ValidationConstants.MAX_AMOUNT_OF_TAGS + 1, "tag"));
+
+        assertThrows(WrongCountOfTagsException.class,
+                () -> validator.isValid(request, null),
+                "Expected WrongCountOfTagsException to be thrown");
+    }
+
+    @Test
+    void isValidMaxCountOfTagsTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setTags(Collections.nCopies(ValidationConstants.MAX_AMOUNT_OF_TAGS, "tag"));
+
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidWithValidTagsTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setTags(Arrays.asList("News", "Events", "Initiatives"));
+
+        assertTrue(validator.isValid(request, null));
+    }
+
+    @Test
+    void isValidWithEmptyTagsTest() {
+        AddEcoNewsDtoRequest request = getAddEcoNewsDtoRequest();
+        request.setTags(Collections.emptyList());
+
+        assertThrows(WrongCountOfTagsException.class,
+                () -> validator.isValid(request, null),
+                "Expected WrongCountOfTagsException to be thrown");
+    }
+
 }
