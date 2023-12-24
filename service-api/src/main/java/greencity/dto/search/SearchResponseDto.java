58cd5bbce21ea04107559da41715f201f1ca860d
Yurii Osovskyi <urio999@gmail.com>
2023-12-24 11:57:31 +0200

    commit_searchcontrollertest_cleaned commented code

--- service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
+++ service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
@@ -1,25 +1,10 @@
 package greencity.dto.search;
 
-//import lombok.Builder;
-//import lombok.EqualsAndHashCode;
-//import lombok.Getter;
-//
-//import java.util.List;
-//
-//@Getter
-//@Builder
-//@EqualsAndHashCode
-//public class SearchResponseDto {
-//    List<SearchNewsDto> ecoNews;
-//
-//    Long countOfResults;
-//}
-
 import lombok.Builder;
-        import lombok.EqualsAndHashCode;
-        import lombok.Getter;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
-        import java.util.List;
+import java.util.List;
 
 @Getter
 @Builder
