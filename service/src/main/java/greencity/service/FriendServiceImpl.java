8cc232dda8a743a481a7b7f669d71ad690974518
lubchenkodenys <denyslubchenko@gmail.com>
2024-01-02 13:43:30 +0200

    update transactional annotation

--- service/src/main/java/greencity/service/FriendServiceImpl.java
+++ service/src/main/java/greencity/service/FriendServiceImpl.java
@@ -1,14 +1,14 @@
 package greencity.service;
 
 import greencity.repository.UserRepo;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class FriendServiceImpl implements FriendService {
     private final UserRepo userRepo;
