49231a673cf56522413483a892918c0992500061
Vlada <proskurina.vlada@gmail.com>
2024-01-04 15:33:37 +0200

    Refactor

--- service-api/src/main/java/greencity/service/FriendService.java
+++ service-api/src/main/java/greencity/service/FriendService.java
@@ -1,6 +1,8 @@
 package greencity.service;
 
 import greencity.dto.PageableDto;
+import greencity.dto.habit.HabitAssignDto;
+import greencity.dto.habit.HabitAssignManagementDto;
 import greencity.dto.user.UserFriendDto;
 import org.springframework.data.domain.Pageable;
 
@@ -11,5 +13,12 @@ import org.springframework.data.domain.Pageable;
  * @version 1.0
  */
 public interface FriendService {
+    /**
+     * Method to find {@code User} by habitAssignId, userId and specific.
+     *
+     * @param userId        {@code User} id.
+     * @param pageable  {@code HabitAssign} id.
+     * @return {@link PageableDto}.
+     */
     PageableDto<UserFriendDto> findAllUsersFriends(Long userId, Pageable pageable);
 }
