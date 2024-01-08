de800d4a6088b1344b61891b75c18820044ea722
Staaankey <dimaclopov@gmail.com>
2024-01-08 13:24:29 +0200

    test for SearchResponseDto.java

--- service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
+++ service-api/src/main/java/greencity/dto/search/SearchResponseDto.java
@@ -13,21 +13,6 @@ public class SearchResponseDto {
     private final List<SearchNewsDto> ecoNews;
     private final Long countOfResults;
 
-
-    // Make the constructor private to enforce the use of the builder pattern
-
-    /**
-     * Constructor.
-     * @param ecoNews constructor parameter.
-     * @param countOfResults constructor parameter.
-     */
-    private SearchResponseDto(List<SearchNewsDto> ecoNews, Long countOfResults) {
-        this.ecoNews = ecoNews;
-        this.countOfResults = countOfResults;
-    }
-
-    // Add a public static method to create an instance using the builder
-
     /**
      * Builder for creating SearchResponseDto.java class
      * @param ecoNews all new that is found for user request.
