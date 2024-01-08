faf4ba0d824971a8e46c85f96489018da8b2e252
Vlada <proskurina.vlada@gmail.com>
2024-01-08 19:18:37 +0200

    Create event services save method

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -1,6 +1,13 @@
 package greencity.service;
 
-public interface EventService {
+import greencity.dto.event.AddEventDto;
+import greencity.dto.event.EventDto;
+import greencity.dto.user.UserVO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+public interface EventService {
+    EventDto save (AddEventDto addEventDto, UserVO userVO, List<MultipartFile> images);
 
 }
--- service/src/main/java/greencity/service/EventServiceImpl.java
+++ service/src/main/java/greencity/service/EventServiceImpl.java
@@ -1,10 +1,27 @@
 package greencity.service;
 
+import greencity.constant.AppConstant;
+import greencity.dto.event.AddEventDto;
+import greencity.dto.event.EventDto;
+import greencity.dto.tag.TagVO;
+import greencity.dto.user.UserVO;
+import greencity.entity.Event;
+import greencity.entity.Tag;
+import greencity.entity.User;
+import greencity.enums.TagType;
 import greencity.repository.EventRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.reflect.TypeUtils;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -13,6 +30,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class EventServiceImpl implements EventService {
 
     private final EventRepo eventRepo;
+    private final ModelMapper modelMapper;
+    private final FileService fileService;
+    private final TagsService tagsService;
 
+    @Override
+    public EventDto save(AddEventDto addEventDto, UserVO userVO, List<MultipartFile> images) {
+        Event event = modelMapper.map(addEventDto, Event.class);
+        event.setCreationDate(LocalDate.now());
+        event.setOrganizer(modelMapper.map(userVO, User.class));
 
+        List<TagVO> tagsVO = tagsService.findTagsByNamesAndType(addEventDto.getTags(), TagType.EVENT);
+        event.setTags(modelMapper.map(tagsVO, TypeUtils.parameterize(List.class, Tag.class)));
+
+        if (images == null || images.isEmpty()) {
+            event.setTitleImage(AppConstant.DEFAULT_HABIT_IMAGE);
+        } else {
+            List<String> imagesUrl = images
+                    .stream()
+                    .filter(Objects::nonNull)
+                    .map(fileService::upload)
+                    .collect(Collectors.toList());
+            event.setTitleImage(imagesUrl.get(0));
+            event.setImages(imagesUrl);
+        }
+        return modelMapper.map(eventRepo.save(event), EventDto.class);
+    }
 }
