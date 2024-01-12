35df8a4e8912450eda5c1c74ef93cf7ad87fe624
Nikita Malov <nikita.volam@gmail.com>
2024-01-12 22:26:57 +0200

    add filter functionality

--- dao/src/main/java/greencity/repository/EcoNewsSearchRepo.java
+++ dao/src/main/java/greencity/repository/EcoNewsSearchRepo.java
@@ -2,19 +2,21 @@ package greencity.repository;
 
 import greencity.entity.EcoNews;
 import greencity.entity.Tag;
+import greencity.entity.User;
 import greencity.entity.localization.TagTranslation;
+import java.util.*;
+import javax.persistence.Tuple;
+import org.hibernate.annotations.QueryHints;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Repository
 public class EcoNewsSearchRepo {
@@ -40,18 +42,40 @@ public class EcoNewsSearchRepo {
         CriteriaQuery<EcoNews> criteriaQuery =
             criteriaBuilder.createQuery(EcoNews.class);
         Root<EcoNews> root = criteriaQuery.from(EcoNews.class);
+        searchingText = formatSearchingText(searchingText);
 
         Predicate predicate = getPredicate(criteriaQuery, searchingText, languageCode, root);
 
-        criteriaQuery.select(root).distinct(true).where(predicate).orderBy(
-            QueryUtils.toOrders(pageable.getSort(), root, criteriaBuilder));
+        List<Order> orderList = getOrderListFromPageable(pageable, root);
+
+        criteriaQuery.select(root)
+            .distinct(true)
+            .where(predicate)
+            .orderBy(orderList);
 
         TypedQuery<EcoNews> typedQuery = entityManager.createQuery(criteriaQuery);
         typedQuery.setFirstResult(pageable.getPageNumber() * pageable.getPageSize());
         typedQuery.setMaxResults(pageable.getPageSize());
+        typedQuery.setHint(QueryHints.PASS_DISTINCT_THROUGH, false);
         List<EcoNews> resultList = typedQuery.getResultList();
 
         return new PageImpl<>(resultList, pageable, getEcoNewsCount(predicate));
+    }
+
+    private List<Order> getOrderListFromPageable(Pageable pageable, Root<EcoNews> root) {
+        List<Order> orderList = new ArrayList<>();
+        pageable.getSort()
+            .get().filter(order -> !order.getProperty().equalsIgnoreCase("relevance"))
+            .forEach(o -> {
+                if (o.getProperty().equalsIgnoreCase("relevance")) {
+                    orderList.add(criteriaBuilder.desc(criteriaBuilder.size(root.get("usersLikedNews"))));
+                } else {
+                    orderList.add(o.isAscending()
+                        ? criteriaBuilder.asc(root.get(o.getProperty()))
+                        : criteriaBuilder.desc(root.get(o.getProperty())));
+                }
+            });
+        return orderList;
     }
 
     private long getEcoNewsCount(Predicate predicate) {
@@ -61,13 +85,13 @@ public class EcoNewsSearchRepo {
         return entityManager.createQuery(countQuery).getSingleResult();
     }
 
-    private List<Predicate> formEcoNewsLikePredicate(String searcingText, Root<EcoNews> root) {
+    private List<Predicate> formEcoNewsLikePredicate(String searchingText, Root<EcoNews> root) {
         Expression<String> title = root.get("title").as(String.class);
         Expression<String> text = root.get("text").as(String.class);
         Expression<String> shortInfo = root.get("shortInfo").as(String.class);
 
         List<Predicate> predicateList = new ArrayList<>();
-        Arrays.stream(searcingText.split(" ")).forEach(partOfSearchingText -> predicateList.add(
+        Arrays.stream(searchingText.split(" ")).forEach(partOfSearchingText -> predicateList.add(
             criteriaBuilder.or(
                 criteriaBuilder.like(criteriaBuilder.lower(title), "%" + partOfSearchingText.toLowerCase() + "%"),
                 criteriaBuilder.like(criteriaBuilder.lower(text), "%" + partOfSearchingText.toLowerCase() + "%"),
@@ -77,7 +101,7 @@ public class EcoNewsSearchRepo {
     }
 
     private Predicate formTagTranslationsPredicate(CriteriaQuery<EcoNews> criteriaQuery, String searchingText,
-        String languageCode, Root<EcoNews> root) {
+                                                   String languageCode, Root<EcoNews> root) {
         Subquery<Tag> tagSubquery = criteriaQuery.subquery(Tag.class);
         Root<Tag> tagRoot = tagSubquery.from(Tag.class);
         Join<EcoNews, Tag> ecoNewsTagJoin = tagRoot.join("ecoNews");
@@ -96,7 +120,7 @@ public class EcoNewsSearchRepo {
     }
 
     private Predicate predicateForTags(String searchingText, String languageCode,
-        Join<TagTranslation, Tag> tagTranslationTagJoin) {
+                                       Join<TagTranslation, Tag> tagTranslationTagJoin) {
         List<Predicate> predicateList = new ArrayList<>();
         Arrays.stream(searchingText.split(" ")).forEach(partOfSearchingText -> predicateList.add(criteriaBuilder.and(
             criteriaBuilder.like(criteriaBuilder.lower(tagTranslationTagJoin.get("name")),
@@ -107,9 +131,17 @@ public class EcoNewsSearchRepo {
     }
 
     private Predicate getPredicate(CriteriaQuery<EcoNews> criteriaQuery, String searchingText,
-        String languageCode, Root<EcoNews> root) {
+                                   String languageCode, Root<EcoNews> root) {
         List<Predicate> predicateList = formEcoNewsLikePredicate(searchingText, root);
         predicateList.add(formTagTranslationsPredicate(criteriaQuery, searchingText, languageCode, root));
         return criteriaBuilder.or(predicateList.toArray(new Predicate[0]));
+    }
+
+    private String formatSearchingText(String criteria) {
+        return criteria
+            .trim()
+            .replace("_", "\\_")
+            .replace("%", "\\%")
+            .replace("\\", "\\\\");
     }
 }
