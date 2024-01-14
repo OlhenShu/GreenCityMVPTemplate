8b4dbbcf0d9905c065bea34ee5ed661e831b7369
Bogdan <veremienko.bogdan@gmail.com>
2024-01-14 21:02:43 +0200

    Fix checkstyle errors

--- service-api/src/main/java/greencity/service/EventService.java
+++ service-api/src/main/java/greencity/service/EventService.java
@@ -4,6 +4,18 @@ package greencity.service;
 import greencity.dto.event.EventVO;
 
 public interface EventService {
+    /**
+     * Method for deleting Event instance.
+     *
+     * @param eventId - event id.
+     * @param email   - user email.
+     */
     void delete(Long eventId, String email);
+
+    /**
+     * Get event by id.
+     *
+     * @param eventId - id of event
+     */
     EventVO findById(Long eventId);
 }
