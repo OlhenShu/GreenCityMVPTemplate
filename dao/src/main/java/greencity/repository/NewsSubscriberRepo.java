cecbfbed03b964a15df8ff67d204355d5b277c92
lubchenkodenys <denyslubchenko@gmail.com>
2024-01-19 14:29:28 +0200

    update findall news subscribers to return stream

--- dao/src/main/java/greencity/repository/NewsSubscriberRepo.java
+++ dao/src/main/java/greencity/repository/NewsSubscriberRepo.java
@@ -1,7 +1,6 @@
 package greencity.repository;
 
 import greencity.entity.NewsSubscriber;
-import java.util.List;
 import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +12,7 @@ public interface NewsSubscriberRepo extends JpaRepository<NewsSubscriber, Long> 
      * @return A list of email addresses of all news subscribers.
      */
     @Query("select ns.email from NewsSubscriber ns")
-    List<String> findAllBy();
+    Stream<String> findAllBy();
 
     /**
      * Checks if a news subscriber with the given email address exists.
