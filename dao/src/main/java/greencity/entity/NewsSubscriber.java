af1f30d2f28846fbca98b5eeb98fd7120a8ee0b4
Nikita Malov <nikita.volam@gmail.com>
2024-01-19 10:31:05 +0200

    add entity

---
+++ dao/src/main/java/greencity/entity/NewsSubscriber.java
@@ -0,0 +1,20 @@
+package greencity.entity;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "news_subscribers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class NewsSubscriber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
