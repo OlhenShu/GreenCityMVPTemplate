dafcd3af22965a6eeb964134e14cafd5fd4f8ff5
Staaankey <dimaclopov@gmail.com>
2024-01-22 15:05:26 +0200

    basic changes

--- core/src/main/java/greencity/config/TelegramBotConfig.java
+++ core/src/main/java/greencity/config/TelegramBotConfig.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Contact;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -25,12 +26,10 @@ public class TelegramBotConfig extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
             SendMessage message = SendMessage.builder()
                     .chatId(chatId)
-                    .text(messageText)
+                    .text("Ви відправили: " + update.getMessage().getText())
                     .build();
             try {
                 execute(message);
